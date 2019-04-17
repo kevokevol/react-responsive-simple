9a6f765aaf061a79c47b6e8506586980ec79c3fa
Kevin Li <kevinl94303spam@gmail.com>
2019-04-17 01:01:31 -0400

    added transpiler, auto prepublish script

---
+++ dist/App.js
@@ -0,0 +1,48 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.Desktop = exports.MobileAndTablet = exports.Tablet = exports.Mobile = undefined;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+require('./App.css');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Mobile(props) {
+    return _react2.default.createElement(
+        'div',
+        { className: 'hidden-sm hidden-md hidden-lg' },
+        props.children
+    );
+}
+function Tablet(props) {
+    return _react2.default.createElement(
+        'div',
+        { className: 'hidden-xs hidden-md hidden-lg' },
+        props.children
+    );
+}
+function MobileAndTablet(props) {
+    return _react2.default.createElement(
+        'div',
+        { className: 'hidden-md hidden-lg' },
+        props.children
+    );
+}
+function Desktop(props) {
+    return _react2.default.createElement(
+        'div',
+        { className: 'hidden-xs hidden-sm' },
+        props.children
+    );
+}
+
+exports.Mobile = Mobile;
+exports.Tablet = Tablet;
+exports.MobileAndTablet = MobileAndTablet;
+exports.Desktop = Desktop;
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.Desktop = exports.MobileAndTablet = exports.Tablet = exports.Mobile = undefined;
+
+var _App = require('./App');
+
+exports.Mobile = _App.Mobile;
+exports.Tablet = _App.Tablet;
+exports.MobileAndTablet = _App.MobileAndTablet;
+exports.Desktop = _App.Desktop;
